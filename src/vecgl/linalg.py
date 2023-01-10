02bc8a4fc46861e5703c1345a28826b16277293e
Frederik Gossen <frgossen@google.com>
2023-01-10 14:40:07 -0500

    Extend linalg

--- src/vecgl/linalg.py
+++ src/vecgl/linalg.py
@@ -1,17 +1,122 @@
 from functools import reduce
-from math import cos, sin
-from typing import List, Tuple, Union
+from math import acos, copysign, cos, inf, sin, sqrt
+import operator
+from typing import Callable, List, Tuple, Union
 
+Vec2 = Tuple[float, float]
 Vec3 = Tuple[float, float, float]
 Vec4 = Tuple[float, float, float, float]
+Mat2 = Tuple[Vec2, Vec2]
 Mat3 = Tuple[Vec3, Vec3, Vec3]
 Mat4 = Tuple[Vec4, Vec4, Vec4, Vec4]
+
+
+kDefaultEps = 0.000000001
+
+
+# 2D cartesian coordinates.
+
+
+def x_vec2(u: Vec2) -> float:
+    ux, _ = u
+    return ux
+
+
+def y_vec2(u: Vec2) -> float:
+    _, uy = u
+    return uy
+
+
+def str_vec2(u: Vec2) -> str:
+    ux, uy = u
+    return f"({ux:.2f}, {uy:.2f})"
+
+
+def _cwise_unary_vec2(fn: Callable[[float], float], u: Vec2):
+    ux, uy = u
+    return fn(ux), fn(uy)
+
+
+def _cwise_binary_vec2(fn: Callable[[float, float], float], u: Vec2, v: Vec2):
+    ux, uy = u
+    vx, vy = v
+    return fn(ux, vx), fn(uy, vy)
+
+
+def _cwise_nary_vec2(fn: Callable[[float, float], float], *us: Vec2) -> Vec2:
+    return reduce(lambda u, v: _cwise_binary_vec2(fn, u, v), us)
+
+
+def uniform_vec2(a: float) -> Vec2:
+    return a, a
+
+
+def is_eps_eq_vec2(u: Vec2, v: Vec2, eps: float = kDefaultEps):
+    ux, uy = u
+    vx, vy = v
+    return abs(ux-vx) < eps and abs(uy-vy) < eps
+
+
+def is_null_vec2(u: Vec2, eps: float = kDefaultEps) -> bool:
+    return is_eps_eq_vec2(u, uniform_vec2(0), eps)
+
+
+def scale_vec2(a: float, u: Vec2) -> Vec2:
+    return _cwise_unary_vec2(lambda ui: a*ui, u)
+
+
+def add_vec2(*us: Vec2) -> Vec2:
+    return _cwise_nary_vec2(operator.add, *us)
+
+
+def sub_vec2(*us: Vec2) -> Vec2:
+    return _cwise_nary_vec2(operator.sub, *us)
+
+
+def dot_vec2(u: Vec2, v: Vec2) -> float:
+    ux, uy = u
+    vx, vy = v
+    return ux * vx + uy * vy
+
+
+def min_vec2(*us: Vec2) -> Vec2:
+    return _cwise_nary_vec2(min, *us)
+
+
+def max_vec2(*us: Vec2) -> Vec2:
+    return _cwise_nary_vec2(max, *us)
+
+
+def mean_vec2(*us: Vec2) -> Vec2:
+    return scale_vec2(1.0 / len(us), add_vec2(*us))
+
+
+def norm2_vec2(u: Vec2) -> float:
+    return sqrt(dot_vec2(u, u))
+
+
+def norm1_vec2(u: Vec2) -> float:
+    ux, uy = u
+    return abs(ux) + abs(uy)
+
+
+def angle_vec2(u: Vec2, v: Vec2) -> float:
+    return acos(dot_vec2(u, v) / (norm2_vec2(u) * norm2_vec2(v)))
+
+
+def unit_vec2(u: Vec2) -> Vec2:
+    return scale_vec2(1.0 / norm2_vec2(u), u)
+
+
+# 3D cartesian coordinates.
 
 
 def to_vec3(u: Union[Vec3, Vec4]) -> Vec3:
     if len(u) == 3:
         return u
     ux, uy, uz, uw = u
+    if uw == 0.0:
+        return copysign(inf, ux), copysign(inf, uy), copysign(inf, uz)
     return ux / uw, uy / uw, uz / uw
 
 
@@ -35,21 +140,45 @@ def str_vec3(u: Vec3) -> str:
     return f"({ux:.2f}, {uy:.2f}, {uz:.2f})"
 
 
+def _cwise_unary_vec3(fn: Callable[[float], float], u: Vec3):
+    ux, uy, uz = u
+    return fn(ux), fn(uy), fn(uz)
+
+
+def _cwise_binary_vec3(fn: Callable[[float, float], float], u: Vec3, v: Vec3):
+    ux, uy, uz = u
+    vx, vy, vz = v
+    return fn(ux, vx), fn(uy, vy), fn(uz, vz)
+
+
+def _cwise_nary_vec3(fn: Callable[[float, float], float], *us: Vec3) -> Vec3:
+    return reduce(lambda u, v: _cwise_binary_vec3(fn, u, v), us)
+
+
+def uniform_vec3(a: float) -> Vec3:
+    return a, a, a
+
+
+def is_eps_eq_vec3(u: Vec3, v: Vec3, eps: float = kDefaultEps):
+    ux, uy, uz = u
+    vx, vy, vz = v
+    return abs(ux-vx) < eps and abs(uy-vy) < eps and abs(uz-vz) < eps
+
+
+def is_null_vec3(u: Vec3, eps: float = kDefaultEps) -> bool:
+    return is_eps_eq_vec3(u, uniform_vec3(0), eps)
+
+
 def scale_vec3(a: float, u: Vec3) -> Vec3:
-    ux, uy, uz = u
-    return a * ux, a * uy, a * uz
+    return _cwise_unary_vec3(lambda ui: a * ui, u)
 
 
-def add_vec3(u: Vec3, v: Vec3) -> Vec3:
-    ux, uy, uz = u
-    vx, vy, vz = v
-    return ux + vx, uy + vy, uz + vz
+def add_vec3(*us: Vec3) -> Vec3:
+    return _cwise_nary_vec3(operator.add, *us)
 
 
-def sub_vec3(u: Vec3, v: Vec3) -> Vec3:
-    ux, uy, uz = u
-    vx, vy, vz = v
-    return ux - vx, uy - vy, uz - vz
+def sub_vec3(*us: Vec3) -> Vec3:
+    return _cwise_nary_vec3(operator.sub, *us)
 
 
 def dot_vec3(u: Vec3, v: Vec3) -> float:
@@ -64,16 +193,36 @@ def cross_vec3(u: Vec3, v: Vec3) -> Vec3:
     return uy * vz - uz * vy, uz * vx - ux * vz, ux * vy - uy * vx
 
 
-def min_vec3(u: Vec3, v: Vec3) -> Vec3:
-    ux, uy, uz = u
-    vx, vy, vz = v
-    return min(ux, vx), min(uy, vy), min(uz, vz)
+def min_vec3(*us: Vec3) -> Vec3:
+    return _cwise_nary_vec3(min, *us)
 
 
-def max_vec3(u: Vec3, v: Vec3) -> Vec3:
+def max_vec3(*us: Vec3) -> Vec3:
+    return _cwise_nary_vec3(max, *us)
+
+
+def mean_vec3(*us: Vec3) -> Vec3:
+    return scale_vec3(1.0 / len(us), add_vec3(*us))
+
+
+def norm2_vec3(u: Vec3) -> float:
+    return sqrt(dot_vec3(u, u))
+
+
+def norm1_vec3(u: Vec3) -> float:
     ux, uy, uz = u
-    vx, vy, vz = v
-    return max(ux, vx), max(uy, vy), max(uz, vz)
+    return abs(ux) + abs(uy) + abs(uz)
+
+
+def angle_vec3(u: Vec3, v: Vec3) -> float:
+    return acos(dot_vec3(u, v) / (norm2_vec3(u) * norm2_vec3(v)))
+
+
+def unit_vec3(u: Vec3) -> Vec3:
+    return scale_vec3(1.0 / norm2_vec3(u), u)
+
+
+# Homogenious coordinates.
 
 
 def to_vec4(u: Union[Vec3, Vec4]) -> Vec4:
@@ -118,21 +267,22 @@ def mul_mat4_vec4(U: Mat4, v: Vec4) -> Vec4:
     return tuple([dot_vec4(u, v) for u in U])
 
 
-def mul_mat4(*Us: Mat4) -> Mat4:
-    def mul_mat4_mat4(U: Mat4, V: Mat4) -> Mat4:
-        l = len(U)
-        m = len(V)
-        n = len(V[0])
-        W: List[Tuple[float, float, float, float]] = []
-        for i in range(l):
-            Wi: List[float] = []
-            for j in range(n):
-                Wij = sum([U[i][k] * V[k][j] for k in range(m)])
-                Wi.append(Wij)
-            W.append(tuple(Wi))
-        return tuple(W)
+def _mul_mat4(U: Mat4, V: Mat4) -> Mat4:
+    l = len(U)
+    m = len(V)
+    n = len(V[0])
+    W: List[Tuple[float, float, float, float]] = []
+    for i in range(l):
+        Wi: List[float] = []
+        for j in range(n):
+            Wij = sum([U[i][k] * V[k][j] for k in range(m)])
+            Wi.append(Wij)
+        W.append(tuple(Wi))
+    return tuple(W)
 
-    return reduce(mul_mat4_mat4, Us)
+
+def mul_mat4(*Us: Mat4) -> Mat4:
+    return reduce(_mul_mat4, Us)
 
 
 def get_unit_mat4() -> Mat4:
